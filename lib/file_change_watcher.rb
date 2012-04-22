44bdfc6768f5deefb9ca2e0c4e4cf4ccdcd0f82d
Robert Feldt <robert.feldt@gmail.com>
2012-04-22 16:44:22 +0200

    copied in file_change_watcher since I need that right away. Will gradually copy in rest of my old and general ruby code that is laying around...

---
+++ lib/file_change_watcher.rb
@@ -0,0 +1,84 @@
+# Watch directories for changes and provide callback hooks for when files change.
+# Partly inspired by and built on code from Autotest/Zentest by Ryan Davis and Eric Hodel.
+#
+require 'find'
+
+class String
+	def starts_with?(str)
+		self[0, str.length] == str
+	end
+end
+
+# Watch for file changes in given paths then call hooks with the updated files.
+class FileChangeWatcher
+	attr_accessor :find_directories, :last_mtime, :sleep_time
+
+	def initialize(specifiedDirectories = ["."], sleepTime = 5*60, &runWhenFilesUpdated)
+		specified_directories  = specifiedDirectories.reject { |path| path.starts_with?("-") }
+    	self.find_directories  = specified_directories.empty? ? ['.'] : specified_directories
+    	self.sleep_time = sleepTime
+    	self.last_mtime = Time.new(0) # Ensure we run first time when started
+    	@hooks = Hash.new { |h,k| h[k] = [] }
+    	add_hook :updated, &runWhenFilesUpdated if runWhenFilesUpdated
+	end
+
+	# Find the files to process, ignoring temporary files, source
+	# configuration management files, etc., and return a Hash mapping
+	# filename to modification time.
+	def find_files
+	  result = {}
+	  targets = self.find_directories
+	  targets.each do |target|
+	    Find.find target do |f|
+	      next if test ?d, f
+	      next if f =~ /(swp|~|rej|orig)$/ # temporary/patch files
+	      next if f =~ /^\.\/tmp/          # temporary dir, used by isolate
+	      next if f =~ /\/\.?#/            # Emacs autosave/cvs merge files
+	      filename = f.sub(/^\.\//, '')
+	      result[filename] = File.stat(filename).mtime rescue next
+	    end
+	  end
+	  result
+  	end
+
+  	# Find files that has changed since last time. 
+  	# Call updated hook if any found.
+  	def find_updated_files files = find_files
+  		#puts "find_updated_files: " + self.find_directories.inspect + ", " + files.length.to_s
+    	updated = files.select { |filename, mtime| self.last_mtime < mtime }
+  		#puts "  updated = " + updated.inspect 
+
+    	# nothing to update or initially run
+    	unless updated.empty? || self.last_mtime.to_i < 0 then
+      		self.last_mtime = Time.now
+      		hook :updated, updated
+    	end
+	end
+
+	# Add the supplied block to the available hooks, with the given
+	# name.
+	def add_hook name, &block
+		# New hooks added in front
+		@hooks[name] = [block] + @hooks[name]
+  	end
+
+	# Call the event hook named +name+, passing in optional args
+	# depending on the hook itself.
+	#
+	# Returns false if no hook handled the event.
+	#
+	def hook name, *args
+	  @hooks[name].any? { |plugin| plugin[self, *args] }
+	end
+
+	def wait_for_changes
+      hook :waiting
+      Kernel.sleep self.sleep_time until find_updated_files
+    end
+end
+
+if __FILE__ == $0
+	dirs_to_watch = ARGV.length > 0 ? ARGV : ["."]
+	watcher = FileChangeWatcher.new(dirs_to_watch) {|fwc, ufs| puts ufs.inspect}
+	watcher.wait_for_changes
+end
