98c853c2de67cff6783490f44fcf8f971f44e4e4
Robert Feldt <robert.feldt@gmail.com>
2012-07-02 10:19:33 +0200

    added SWGR calculation and tests

--- lib/feldtruby/optimize/objective.rb
+++ lib/feldtruby/optimize/objective.rb
@@ -24,13 +24,62 @@ class FeldtRuby::Optimize::Objective
 	# By default this uses Bentley and Wakefield's sum-of-weighted-global-ratios (SWGR) 
 	# if there are multiple aspects/sub-objectives. If this is a single objective we 
 	# just return the value of the objective_X method.
-	def quality_value(candidate)
-		num_aspects == 1 ? qv_single(candidate) : qv_swgr(candidate)
+	def quality_value(candidate, weights = nil)
+		num_aspects == 1 ? qv_single(candidate) : qv_swgr(candidate, weights)
 	end
 
 	# Return the quality value assuming this is a single objective.
 	def qv_single(candidate)
 		self.send(aspect_methods.first, candidate)
+	end
+
+	# Sum-of-weigthed-global-ratios (SWGR) quality value
+	def qv_swgr(candidate, weights = nil)
+		sub_qvs = sub_objective_values(candidate)
+		update_global_mins_and_maxs(sub_qvs)
+		swgr_ratios(sub_qvs).weighted_sum(weights)
+	end
+
+	# Calculate the SWGR ratios
+	def swgr_ratios(subObjectiveValues)
+		subObjectiveValues.each_with_index.map {|v,i| ratio_for_aspect(i, v)}
+	end
+
+	def ratio_for_aspect(aspectIndex, value)
+		min, max = global_min_values_per_aspect[aspectIndex], global_max_values_per_aspect[aspectIndex]
+		if is_min_aspect?(aspectIndex)
+			numerator = max - value
+		else
+			numerator = value - min
+		end
+		numerator / (max - min)
+	end
+
+	def sub_objective_values(candidate)
+		aspect_methods.map {|omethod| self.send(omethod, candidate)}
+	end
+
+	def update_global_mins_and_maxs(aspectValues)
+		aspectValues.each_with_index {|v, i| update_global_min_and_max(i, v)}
+	end
+
+	def update_global_min_and_max(aspectIndex, value)
+		global_min_values_per_aspect[aspectIndex] = [global_min_values_per_aspect[aspectIndex], value].min
+		global_max_values_per_aspect[aspectIndex] = [global_max_values_per_aspect[aspectIndex], value].max
+	end
+
+	# Global min values for each aspect. Needed for SWGR. Updated every time we see a new
+	# quality value for an aspect.
+	# All are minus infinity when we have not seen any values yet.
+	def global_min_values_per_aspect
+		@global_min_values_per_aspect ||= Array.new(num_aspects).map {Float::INFINITY}
+	end
+
+	# Global max values for each aspect. Needed for SWGR. Updated every time we see a new
+	# quality value for an aspect.
+	# All are minus infinity when we have not seen any values yet.
+	def global_max_values_per_aspect
+		@global_max_values_per_aspect ||= Array.new(num_aspects).map {-Float::INFINITY}
 	end
 
 	private
@@ -39,7 +88,15 @@ class FeldtRuby::Optimize::Objective
 		@aspect_methods ||= self.methods.select {|m| is_aspect_method?(m)}
 	end
 
+	def is_min_aspect?(aspectIndex)
+		(@is_min_aspect ||= (aspect_methods.map {|m| is_min_aspect_method?(m)}))[aspectIndex]
+	end
+
 	def is_aspect_method?(methodNameAsSymbolOrString)
-		methodNameAsSymbolOrString.to_s =~ /^objective_(min)|(max)_([\w_]+)$/
+		methodNameAsSymbolOrString.to_s =~ /^objective_(min|max)_([\w_]+)$/
+	end
+
+	def is_min_aspect_method?(methodNameAsSymbolOrString)
+		methodNameAsSymbolOrString.to_s =~ /^objective_min_([\w_]+)$/
 	end
 end
--- test/test_optimize_objective.rb
+++ test/test_optimize_objective.rb
@@ -1,5 +1,7 @@
 require 'feldtruby/optimize/objective'
-require 'feldtruby/array/basic_stats'
+require 'feldtruby/array'
+
+require 'pp'
 
 class SingleObjective1 < FeldtRuby::Optimize::Objective
 	# Sum of candidate vector of values should be small
@@ -24,14 +26,52 @@ class TestSingleObjective < MiniTest::Unit::TestCase
 	end
 end
 
-class TwoObjectives1 < FeldtRuby::Optimize::Objective
+class TwoMinObjectives1 < FeldtRuby::Optimize::Objective
 	def objective_min_small_distance_between(candidate)
-		candidate.distance_between.sum
+		candidate.distance_between_elements.sum
 	end
-	def objective_max_sum(candidate)
+	def objective_min_sum(candidate)
 		candidate.sum
 	end
 end
 
 class TestTwoObjectives < MiniTest::Unit::TestCase
+	def setup
+		@o = TwoMinObjectives1.new
+	end
+	def test_has_two_aspects
+		assert_equal 2, @o.num_aspects		
+	end
+	def test_global_min_values_per_aspect
+		assert_equal [Float::INFINITY, Float::INFINITY], @o.global_min_values_per_aspect
+	end
+	def test_global_max_values_per_aspect
+		assert_equal [-Float::INFINITY, -Float::INFINITY], @o.global_max_values_per_aspect
+	end
+	def test_update_global_mins_and_maxs
+		@o.update_global_mins_and_maxs([1,2])
+		assert_equal [1,2], @o.global_min_values_per_aspect
+		assert_equal [1,2], @o.global_max_values_per_aspect
+
+		@o.update_global_mins_and_maxs([1,3])
+		assert_equal [1,2], @o.global_min_values_per_aspect
+		assert_equal [1,3], @o.global_max_values_per_aspect
+
+		@o.update_global_mins_and_maxs([0,8])
+		assert_equal [0,2], @o.global_min_values_per_aspect
+		assert_equal [1,8], @o.global_max_values_per_aspect
+	end
+	def test_sub_objective_values
+		assert_equal [1,3], @o.sub_objective_values([1,2])
+		assert_equal [4,8], @o.sub_objective_values([1,2,5])
+	end
+	def test_qv_swgr
+		@o.update_global_mins_and_maxs([0, 0])
+		@o.update_global_mins_and_maxs([1, 3])
+		assert_equal 0.0, @o.qv_swgr([1,2])
+		assert_equal 2.0, @o.qv_swgr([0,0])
+	end
+	def test_qv_swgr_complex
+		assert_equal 0.0, @o.qv_swgr([1,2])
+	end
 end
