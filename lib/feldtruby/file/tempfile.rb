234ca49e19108940a461cd32bfcc626f423b6c2b
Robert Feldt <robert.feldt@gmail.com>
2012-09-15 17:15:41 +0200

    added temp file creation methods

---
+++ lib/feldtruby/file/tempfile.rb
@@ -0,0 +1,25 @@
+# Find a unique temp file name starting with _basename_
+# and having a file suffix _suffix_, then supply it to a block. 
+# After the block has executed make sure there is no tempfile 
+# with that name, if there is delete it.
+def File.with_tempfile(suffix = ".temp", basename = "temp")
+	tempfilename = File.join(basename, rand(1e7).to_s + suffix)
+	while Dir.exist?(tempfilename)
+		tempfilename = File.join(basename, rand(1e7).to_s)
+	end
+	begin
+		yield tempfilename
+	ensure
+		File.delete(tempfilename) if Dir.exist?(tempfilename)
+	end
+end
+
+# Find a unique temp file name in the same way as for with_tempfile
+# but now save a string to that file before supplying the filename
+# to the block. 
+def File.with_tempfile_containing(contents, suffix = ".temp", basename = "temp")
+	File.with_tempfile do |tempfilename|
+		File.open(tempfilename, "w") {|fh| fh.write(contents)}
+		yield tempfilename
+	end
+end
