c94cd4e20c9bf75d7a401f602c46331672ba1cc6
Robert Feldt <robert.feldt@gmail.com>
2013-01-14 15:40:54 +0100

    bail when being loaded since we require Ruby 1.9

--- lib/feldtruby.rb
+++ lib/feldtruby.rb
@@ -1,6 +1,7 @@
-# this is just for Hoe's version numbering
-class Feldtruby
-  VERSION = '0.2.2'
+if RUBY_VERSION < "1.9"
+  puts "ERROR: feldtruby requires Ruby version 1.9"
+  exit(-1)
 end
+
 # This is the namespace under which we put things...
 module FeldtRuby; end
