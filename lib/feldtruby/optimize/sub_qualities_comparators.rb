aba02ecc05a929b307b184aea0fc8cac74dfa292
Robert Feldt <robert.feldt@gmail.com>
2013-03-01 23:52:22 +0100

    started adding nondominance comparators

---
+++ lib/feldtruby/optimize/sub_qualities_comparators.rb
@@ -0,0 +1,57 @@
+module FeldtRuby::Optimize
+
+# A SubQualititesComparator can compare vectors of sub-qualitites for two individuals
+# and rank the individuals based on if one is better (or dominates) the other.
+class SubQualitiesComparator
+  def initialize(objective)
+    @objective = objective
+  end
+  def compare_sub_qualitites(subQualitites1, subQualitites2)
+    raise NotImplementedError
+  end
+  def compare_candidates(candidate1, candidate2)
+    sq1, sq2 = @objective.sub_qualities_of(candidate1), @objective.sub_qualities_of(candidate2)
+    compare_sub_qualitites sq1, sq2
+  end
+end
+
+# Epsilon-distance non-dominance comparator. Default epsilon is 0.0 which
+# gives the standard non-dominance comparator.
+class EpsilonNonDominance < SubQualitiesComparator
+  def initialize(objective, epsilon = 0.0)
+    super(objective)
+    @epsilon = epsilon
+  end
+
+  # Map hat operator to paired sub-quality values.
+  def map_hat_operator(sq1, sq2)
+    # NOTE! Below we assume that all sub-objectives should be minimized. If not we should
+    # change the sign of the hat operator return value!
+    sq1.zip(sq2).map do |sqv1, sqv2|
+      if (sqv1 - sqv2).abs > @epsilon
+        (sqv1 < sqv2) ? -1 : 1
+      else
+        0
+      end
+    end
+  end
+
+  def compare_sub_qualitites(subQualitites1, subQualitites2)
+    hat_values = map_hat_operator(subQualitites1, subQualitites2)
+    num_1_better = num_2_better = 0
+    hat_values.each do |hv|
+      if hv == -1
+        num_1_better += 1
+      elsif hv == 1
+        num_2_better += 1
+      end
+    end
+    if num_1_better > 0
+      (num_2_better == 0) ? -1 : 0
+    else
+      (num_2_better > 0) ? 1 : 0
+    end
+  end
+end
+
+end
---
+++ test/test_sub_qualitites_comparator.rb
@@ -0,0 +1,57 @@
+require 'feldtruby/optimize/sub_qualities_comparators'
+
+describe "EpsilonNonDominance with epsilon=0.0 i.e. normal non-dominance" do
+  before do
+    @c = FeldtRuby::Optimize::EpsilonNonDominance.new(1, 0.0)
+  end
+
+  it "correctly calculates dominance for single-objective examples" do
+    @c.compare_sub_qualitites([1], [1]).must_equal 0
+
+    @c.compare_sub_qualitites([1], [2]).must_equal -1
+    @c.compare_sub_qualitites([2], [1]).must_equal 1
+
+    @c.compare_sub_qualitites([-1], [1]).must_equal -1
+    @c.compare_sub_qualitites([1], [-1]).must_equal 1
+
+    @c.compare_sub_qualitites([-10], [0]).must_equal -1
+    @c.compare_sub_qualitites([0], [-10]).must_equal 1
+  end
+
+  it "correctly calculates dominance for two-objective examples" do
+    @c.compare_sub_qualitites([1, 1], [1, 1]).must_equal 0
+
+    @c.compare_sub_qualitites([1, 2], [1, 3]).must_equal -1
+    @c.compare_sub_qualitites([1, 3], [1, 2]).must_equal 1
+
+    @c.compare_sub_qualitites([2, -1], [3, -1]).must_equal -1
+    @c.compare_sub_qualitites([3, -1], [2, -1]).must_equal 1
+
+    @c.compare_sub_qualitites([1, 2], [2, 1]).must_equal 0
+  end
+
+  it "never shows dominance when comparing the same objects" do
+    100.times do
+      candidate = Array.new(1 + rand(20)).map {rand(1e3)}
+      @c.compare_sub_qualitites(candidate, candidate).must_equal 0
+    end
+  end
+
+  it "always shows dominance when comparing objects where left is better in a single sub-objective" do
+    100.times do
+      candidate1 = Array.new(1 + rand(20)).map {rand(1e3)}
+      candidate2 = candidate1.clone
+      candidate2[rand(candidate2.length)] += 1
+      @c.compare_sub_qualitites(candidate1, candidate2).must_equal -1
+    end
+  end
+
+  it "always shows dominance when comparing objects where right is better in a single sub-objective" do
+    100.times do
+      candidate1 = Array.new(1 + rand(20)).map {rand(1e3)}
+      candidate2 = candidate1.clone
+      candidate2[rand(candidate2.length)] -= 1
+      @c.compare_sub_qualitites(candidate1, candidate2).must_equal 1
+    end
+  end
+end
