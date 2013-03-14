6f559d77a5bc3be6842a335aee751652db39dd6f
Robert Feldt <robert.feldt@gmail.com>
2013-03-14 08:55:40 +0100

    skip network-dependent tests if we have no network connection

--- test/test_html_doc_getter.rb
+++ test/test_html_doc_getter.rb
@@ -1,16 +1,35 @@
 require 'minitest/spec'
 require 'feldtruby/net/html_doc_getter'
 
-describe "HtmlDocGetter" do
-  it "Can get the html page as a string" do
-    h = FeldtRuby::HtmlDocGetter.new
-    s = h.get("http://www.google.com")
-    s.must_be_instance_of String
+# Skip these network-dependent tests if we have no network connection.
+def has_network_connection?
+  begin
+    open("http://www.google.se")
+    return true
+  rescue Exception => e
+    return false
+  end
+end
+
+def quicker_has_network_connection?
+  reply = `ping -o google.se`
+  reply != "" # Is empty if no connection since error is printed on STDERR
+end
+
+if quicker_has_network_connection?
+
+  describe "HtmlDocGetter" do
+    it "Can get the html page as a string" do
+      h = FeldtRuby::HtmlDocGetter.new
+      s = h.get("http://www.google.com")
+      s.must_be_instance_of String
+    end
+  
+    it "Can get the html page as a Nokogiri doc" do
+      h = FeldtRuby::HtmlDocGetter.new
+      d = h.get_html_doc("http://www.google.com")
+      d.must_be_instance_of Nokogiri::HTML::Document
+    end
   end
 
-  it "Can get the html page as a Nokogiri doc" do
-    h = FeldtRuby::HtmlDocGetter.new
-    d = h.get_html_doc("http://www.google.com")
-    d.must_be_instance_of Nokogiri::HTML::Document
-  end
 end
