87eb786f228dce094ae99de250b227750711025a
Robert Feldt <robert.feldt@gmail.com>
2013-04-05 18:20:51 +0200

    old change I had not checked in

--- lib/feldtruby/minitest_extensions.rb
+++ lib/feldtruby/minitest_extensions.rb
@@ -6,7 +6,6 @@ module MiniTest::Assertions
   # Ensure that that are (statistically) the same number of each type
   # of value in an array.
   def assert_similar_proportions(values, expectedPValue = 0.01, msg = nil)
-    #pvalue = FeldtRuby.probability_of_same_proportions(values)
     pvalue = FeldtRuby.chi_squared_test(values)
     assert(pvalue > expectedPValue, msg || "Proportions differ! p-value is #{pvalue} (<0.05), counts: #{values.counts.inspect}")
   end
