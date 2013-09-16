ae1d4308bb4a733569c345a5d743816d2880867e
Robert Feldt <robert.feldt@gmail.com>
2013-09-16 12:31:01 +0200

    switch over the ngd method to use the cache by default

--- lib/feldtruby/statistics/distance/ngd.rb
+++ lib/feldtruby/statistics/distance/ngd.rb
@@ -61,6 +61,8 @@ module FeldtRuby
     end
   end
 
+  DefaultGoogleHitsCache = GoogleHitsCache.new
+
   # Return the number of hits Google reports for a certain selection of search terms.
   def self.num_google_hits_for_search_terms(*searchTerms)
     sts = searchTerms.map do |st|
@@ -80,10 +82,10 @@ module FeldtRuby
 
   # Calculate the NGD (Normalized Google Distance) for two search terms/words.
   def self.ngd(s1, s2)
-    l_the =  Math.log2(num_google_hits_for_search_terms("the"))
-    l1 =  Math.log2(num_google_hits_for_search_terms(s1))
-    l2 =  Math.log2(num_google_hits_for_search_terms(s2))
-    l1and2 = Math.log2(num_google_hits_for_search_terms(s1, s2))
+    l_the =  Math.log2(DefaultGoogleHitsCache.num_hits("the"))
+    l1 =  Math.log2(DefaultGoogleHitsCache.num_hits(s1))
+    l2 =  Math.log2(DefaultGoogleHitsCache.num_hits(s2))
+    l1and2 = Math.log2(DefaultGoogleHitsCache.num_hits(s1, s2))
     ([l1, l2].max - l1and2) / (l_the - [l1, l2].min)
   end
 end
